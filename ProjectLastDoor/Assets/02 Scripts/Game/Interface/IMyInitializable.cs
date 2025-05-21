6585bce1829a07c97898ae5d4bfd1edd22e5a7c7
Johyechan <johyechan6@gmail.com>
2025-05-21 19:02:53 +0900

    Day 3
    1. 작은 상자 오브젝트 임시 제작
     1-1. 자물쇠 오브젝트 임시 제작
     1-2. 키 오브젝트 임시 제작
    2. 주석 달기
    3. 태그 추가

--- ProjectLastDoor/Assets/02 Scripts/Game/Interface/IMyInitializable.cs
+++ ProjectLastDoor/Assets/02 Scripts/Game/Interface/IMyInitializable.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 
+// 인터페이스들을 모아둔 네임스페이스
 namespace Game.Interface
 {
+    // 작성자: 조혜찬
+    // 초기화 기능 구현 인터페이스
     public interface IMyInitializable
     {
         public bool Initialize();
     }
 }
-
+// 마지막 작성 일자: 2025.05.21
